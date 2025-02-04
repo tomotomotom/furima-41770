a92a63e65f101e439d08644451327cf97923f7ca
tomotomotom <torimorinorikeyhome@gmail.com>
2025-02-04 22:28:17 +0900

    ユーザー管理機能実装（改善１）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Internet.password(min_length: 6)+'1a'}
     password_confirmation {password}
     last_name             {"田中"}
     first_name            {"太郎"}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,6 +45,11 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("First name can't be blank", "First name is invalid. Input full-width characters.")    
     end
+    it 'birth_dateが空では登録できない' do
+      @user.birth_date = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birth date can't be blank")    
+    end
     it 'passwordとpassword_confirmationが不一致では登録できない' do
       @user.password = '123456'
       @user.password_confirmation = '1234567'
@@ -74,6 +79,44 @@ RSpec.describe User, type: :model do
       @user.password_confirmation = @user.password
       @user.valid?
       expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
+    end
+    it '英字のみのpasswordでは登録できない' do
+      @user.password = 'aaaaaa'
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid. Input half-width characters.")
+    end
+    it '数字のみのpasswordでは登録できない' do
+      @user.password = '111111'
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid. Input half-width characters.")
+    end
+    it '全角文字を含むpasswordでは登録できない' do
+      @user.password = 'ああああああ'
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid. Input half-width characters.")
+    end
+    it 'last_nameに半角文字が含まれていると登録できない' do
+      @user.last_name = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name is invalid. Input full-width characters.")    
+    end
+    it 'first_nameに半角文字が含まれていると登録できない' do
+      @user.first_name = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name is invalid. Input full-width characters.")    
+    end
+    it 'last_name_kanaにカタカナ以外の文字（ひらがな・漢字・英数字・記号）が含まれていると登録できない' do
+      @user.last_name = 'あ亜a1@'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name is invalid. Input full-width characters.")    
+    end
+    it 'first_name_kanaにカタカナ以外の文字（ひらがな・漢字・英数字・記号）が含まれていると登録できない' do
+      @user.first_name = 'あ亜a1@'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name is invalid. Input full-width characters.")    
     end
   end
   end
