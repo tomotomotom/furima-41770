85f48c9bed692d2dde505b8b9b9bf47e6b653274
tomotomotom <torimorinorikeyhome@gmail.com>
2025-02-15 14:54:42 +0900

    データベース作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_07_222428) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_15_054745) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,6 +54,28 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_07_222428) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "orders", charset: "utf8mb3", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "item_prefecture_id", null: false
+    t.string "city", null: false
+    t.string "addresses", null: false
+    t.string "building", null: false
+    t.string "phone_number", null: false
+    t.bigint "purchase_record_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["purchase_record_id"], name: "index_orders_on_purchase_record_id"
+  end
+
+  create_table "purchase_records", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_purchase_records_on_item_id"
+    t.index ["user_id"], name: "index_purchase_records_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -75,4 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_07_222428) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "purchase_records"
+  add_foreign_key "purchase_records", "items"
+  add_foreign_key "purchase_records", "users"
 end
