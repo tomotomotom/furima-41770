698c7c7cedc92662433431043aa1802fc86ed42b
tomotomotom <torimorinorikeyhome@gmail.com>
2025-02-22 08:05:54 +0900

    コントローラー編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,35 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [ :index, :create]
+  # before_action :set_item, only: [:index, :create]
+
 
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @order = Order.create(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
-  
+
+  private
+  def order_params
+    params.require(:order).permit(
+      :postal_code,
+      :prefecture_id,
+      :city,
+      :addresses,
+      :building,
+      :phone_number
+      ).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  # def set_item
+  #   @item = Item.find(params[:item_id])
+  # end
+
 end
