ec9a1586b22e650b9a36a9b907b05ec7309f24bf
tomotomotom <torimorinorikeyhome@gmail.com>
2025-03-01 18:45:02 +0900

    Revert "クレジットカード導入"
    This reverts commit d53b39528004be31cd96c563b26dad7f594fc09b.

---
+++ app/models/order_form.rb
@@ -0,0 +1,34 @@
+class OrderForm
+  include ActiveModel::Model
+
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
+ 
+  with_options presence: true do
+  validates :postal_code
+  validates :city,format: { with: /\A\d{3}-\d{4}\z/, messaformat: { with: /\A\d{3}-\d{4}\z/, message: "は「000-0000」の形式で入力してください" } }
+  validates :addresses
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁または11桁の半角数字で入力してください" }
+  validates :user_id
+  validates :item_id
+  end
+  validates :prefecture_id,numericality: { other_than: 1 ,message: "can't be blank"}
+
+
+  def save
+    
+    PurchaseRecord.create(
+      user_id: user_id,
+      item_id: item_id
+    )
+
+    order = Order.create(
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      addresses: addresses,
+      building: building,
+      phone_number: phone_number,
+      purchase_record_id: purchase_record.id
+    )
+  end
+end
