e09897f52d1dddf320756f3ae777ef3150345aa2
tomotomotom <torimorinorikeyhome@gmail.com>
2025-03-06 20:01:29 +0900

    カード決済機能実装完了

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -10,28 +10,25 @@ const pay = () => {
     expiryElement.mount('#expiry-form');
     cvcElement.mount('#cvc-form');
     const form = document.getElementById('charge-form')
+
     form.addEventListener("submit", (e) => {
-    e.preventDefault();
       payjp.createToken(numberElement).then(function (response) {
         if (response.error) {
-        console.error(response.error.message);
-        } else {
-          const token = response.id;
-        console.log("Generated token:", token); // トークンの値を確認
-
+        }else{
+        const token = response.id;
         const tokenObj = document.createElement('input');
         tokenObj.setAttribute('type', 'hidden');
         tokenObj.setAttribute('name', 'order_form[token]');
         tokenObj.setAttribute('value', token);
-        form.appendChild(tokenObj);
-
+        renderDom.insertAdjacentHTML(tokenObj);
+        }
         numberElement.clear();
         expiryElement.clear();
         cvcElement.clear();
-        form.submit();
-      }
+        document.getElementById("charge-form").submit();
     });
+    e.preventDefault();
   });
 };
-  window.addEventListener("turbo:load", pay);
-  window.addEventListener("turbo:render", pay);
+window.addEventListener("turbo:load", pay);
+window.addEventListener("turbo:render", pay);
