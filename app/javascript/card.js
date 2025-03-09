4d16b16da6154f821e46562db073e3a676d172b5
tomotomotom <torimorinorikeyhome@gmail.com>
2025-03-09 16:04:45 +0900

    本番環境修正１

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -20,8 +20,8 @@ const pay = () => {
           const renderDom = document.getElementById("charge-form");
           const tokenObj = `<input value="${token}" name='token' type="hidden">`;
           renderDom.insertAdjacentHTML("beforeend", tokenObj);
-          document.getElementById("charge-form").submit();
         }
+        document.getElementById("charge-form").submit();
         numberElement.clear();
         expiryElement.clear();
         cvcElement.clear();
